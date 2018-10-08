c70e92f121fc1684437cf30221f215e3a81c5868
ealcobaca <e.alcobaca@gmail.com>
2018-10-07 21:07:17 -0300

    feature(range): version 1

--- source/range/range_cutter.py
+++ source/range/range_cutter.py
@@ -1,6 +1,8 @@
 from sklearn.model_selection import RepeatedKFold
 import pandas as pd
 import numpy as np
+import os
+import pickle
 from regressors import train_regressors, apply_regressors, compute_performance
 
 SEED=123
@@ -11,7 +13,7 @@ def fill_data(X, y, value, grather=True):
     return X[y <= value], y[y<=value]
 
 
-def evaluate(X, y, rcv, range_high_TG, range_low_TG):
+def evaluate(X, y, alg, rcv, range_high_TG, range_low_TG):
 
     results_perf = []
     results_perf_high = []
@@ -21,8 +23,8 @@ def evaluate(X, y, rcv, range_high_TG, range_low_TG):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
-        train_regr = train_regressors(X_train, y_train)
-        preds = apply_regressors(X_test)
+        train_regr = train_regressors(X_train, y_train, alg, SEED)
+        preds = apply_regressors(train_regr, X_test)
         result_perf = compute_performance(preds, y_test)
 
         results_perf.append(result_perf)
@@ -30,7 +32,7 @@ def evaluate(X, y, rcv, range_high_TG, range_low_TG):
         aux = []
         for data_range in range_high_TG:
             X_range_test, y_range_test = fill_data(X_test, y_test, data_range, True)
-            preds = apply_regressors(X_range_test)
+            preds = apply_regressors(train_regr, X_range_test)
             result_perf = compute_performance(preds, y_range_test)
             aux.append(result_perf)
         results_perf_high.append(aux)
@@ -38,13 +40,14 @@ def evaluate(X, y, rcv, range_high_TG, range_low_TG):
         aux = []
         for data_range in range_low_TG:
             X_range_test, y_range_test = fill_data(X_test, y_test, data_range, False)
-            preds = apply_regressors(X_range_test)
+            preds = apply_regressors(train_regr, X_range_test)
             result_perf = compute_performance(preds, y_range_test)
             aux.append(result_perf)
         results_perf_low.append(aux)
 
+    return results_perf, results_perf_high, results_perf_low
 
-def evaluate_range(X, y, rcv, value, grather):
+def evaluate_range(X, y, alg, rcv, value, grather):
 
     results_perf = []
     for train_index, test_index in rcv.split(X):
@@ -53,8 +56,8 @@ def evaluate_range(X, y, rcv, value, grather):
         X_range_train, y_range_train = fill_data(X_train, y_train, value, grather)
         X_range_test, y_range_test = fill_data(X_test, y_test, value, grather)
 
-        train_regr = train_regressors(X_range_train, y_range_train)
-        preds = apply_regressors(X_range_test)
+        train_regr = train_regressors(X_range_train, y_range_train, alg, SEED)
+        preds = apply_regressors(train_regr, X_range_test)
         result_perf = compute_performance(preds, y_range_test)
 
         results_perf.append(result_perf)
@@ -63,21 +66,52 @@ def evaluate_range(X, y, rcv, value, grather):
 
 
 def run(data_path, str_class):
+    algs = ["DT", "RF", "XG", "SVM", "MLP"]
     data = pd.read_csv(data_path)
     rcv = RepeatedKFold(n_splits=10, n_repeats=100, random_state=SEED)
 
-    range_high_TG = np.arange(start=900, stop=1150, step=25)
-    range_low_TG = np.arange(start=350, stop=650+1, step=25)
+    range_high_TG = np.arange(start=900, stop=1100+1, step=25)
+    range_low_TG = np.arange(start=400, stop=650+1, step=25)
 
     X = data.drop([str_class], axis=1).values
     y = data[str_class].values
 
-    evaluate(X, y, rcv, range_high_TG, range_low_TG)
+    # evaluate(X, y, rcv, range_high_TG, range_low_TG)
+    result_path = "../../result/result_all/"
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
+
+    for alg in algs:
+        print("all - {0}".format(alg))
+        results_perf, results_perf_high, results_perf_low = evaluate(
+            X, y, alg, rcv, range_high_TG, range_low_TG)
+        file_name = result_path+"result_all_"+alg+".csv"
+        pickle.dump(results_perf, open(file_name, "wb" ))
+        file_name = result_path+"result_all_high_"+alg+".csv"
+        pickle.dump(results_perf_high, open(file_name, "wb" ))
+        file_name = result_path+"result_all_low"+alg+".csv"
+        pickle.dump(results_perf_low, open(file_name, "wb" ))
+
+    result_path = "../../result/result_high/"
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
 
     for data_range in range_high_TG:
-        evaluate_range(X, y, rcv, data_range, True)
+        for alg in algs:
+            print("all - {0} - {1}".format(alg, data_range))
+            file_name = result_path+"result_"+str(data_range)+"_"+alg+".csv"
+            result = evaluate_range(X, y, alg, rcv, data_range, True)
+            pickle.dump(result, open(file_name, "wb" ))
+
+    result_path = "../../result/result_low/"
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
 
     for data_range in range_low_TG:
-        evaluate_range(X, y, rcv, data_range, True)
+        for alg in algs:
+            print("all - {0} - {1}".format(alg, data_range))
+            file_name = result_path+"result_"+str(data_range)+"_"+alg+".csv"
+            result = evaluate_range(X, y, rcv, data_range, True)
+            pickle.dump(result, open(file_name, "wb" ))
 
 
--- source/range/regressors.py
+++ source/range/regressors.py
@@ -1,10 +1,86 @@
-def train_regressors(X_range_train, y_range_train):
-    pass
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+import xgboost as xgb
+import numpy as np
 
 
-def apply_regressors(X_range_test):
-    pass
+def RRMSE(target, pred):
+    num = np.sum((target - pred) ** 2)
+    dem = np.sum((np.mean(target) - target) ** 2)
+    return np.sqrt(num/dem)
 
 
-def compute_performance(preds, y_range_test):
-    pass
+def MSE(target, pred):
+    N = len(target)
+    return np.sum((target-pred)**2)/N
+
+
+def RMSE(target, pred):
+    return np.sqrt(MSE(target, pred))
+
+
+def train_regressors(X_train, y_train, regressor, seed):
+    reg = None
+    if regressor == "RF":
+        reg = RandomForestRegressor(
+            n_estimators=100,
+            criterion="mse",
+            max_depth=None,
+            min_samples_split=0.01,
+            min_samples_leaf=0.005,
+            n_jobs=10,
+            random_state=seed).fit(X_train,y_train)
+    elif regressor == "DT":
+        reg = DecisionTreeRegressor(
+            criterion="mse",
+            splitter="best",
+            max_depth=None,
+            min_samples_split=0.01,
+            min_samples_leaf=0.005,
+            random_state=seed).fit(X_train, y_train)
+    elif regressor == "XG":
+        # data = xgb.DMatrix(data=X_train,label=y_train)
+        reg = xgb.XGBRegressor(
+            objective ='reg:linear',
+            n_jobs=10,
+            n_estimators = 100,
+            random_state=seed).fit(X_train, y_train)
+    elif regressor == "SVM":
+        reg = SVR(
+            kernel="rbf",
+            gamma="scale",
+            C=1.0,
+            epsilon=0.1,
+            max_iter=10000000,
+            random_state=seed).fit(X_train, y_train)
+    elif regressor == "MLP":
+        reg = MLPRegressor(
+            hidden_layer_sizes=50,
+            activation="relu",
+            solver="adam",
+            alpha=0.0001,
+            max_iter=800,
+            early_stopping=True,
+            random_state=seed).fit(X_train, y_train)
+    else:
+        print("Error")
+        reg = None
+    return reg
+
+
+def apply_regressors(reg, X_test):
+    return reg.predict(X_test)
+
+
+def compute_performance(y_pred, y_true):
+    result = [mean_absolute_error(y_true, y_pred),
+              mean_squared_error(y_true, y_pred),
+              r2_score(y_true, y_pred),
+              RRMSE(y_true, y_pred),
+              RRMSE(y_true, y_pred)]
+    return result
