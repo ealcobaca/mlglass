4033f88b84dc768518eafd3521e3e8aaed2daacf
ealcobaca <e.alcobaca@gmail.com>
2018-10-09 09:48:50 -0300

    feature(result): new results

---
+++ source/range/pbs.sh
@@ -0,0 +1,14 @@
+#$1 arg 1
+
+#PBS -N glass
+#PBS -l select=1:ncpus=56:nodetype=n56
+#PBS -l walltime=336:00:00
+#PBS -m abe
+#PBS -M e.alcobaca@gmail.com
+
+source predicting_high_low_TG/exp3.5/bin/activate
+cd /home/alcobaca/predicting_high_low_TG/source/range
+
+printf "$(pwd)\n"
+
+python run.py --n_cpus=56 -e 2
