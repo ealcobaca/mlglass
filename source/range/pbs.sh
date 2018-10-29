742d2490b00666b08e1b2268fa9df127d1a2339a
ealcobaca <e.alcobaca@gmail.com>
2018-10-29 02:23:34 -0300

    feature(range): new pbs to run the experiments in parallel

--- source/range/pbs.sh
+++ source/range/pbs.sh
@@ -1,13 +1,54 @@
-#$1 arg 1
+#!/bin/bash
 
-#PBS -N glass
-#PBS -l select=1:ncpus=56:nodetype=n56
-#PBS -l walltime=336:00:00
-#PBS -m abe
-#PBS -M e.alcobaca@gmail.com
+create() {
+    cat > pbs_$2.sh << END
+    module load python/3.5.4
+    cd $1
+    source ../../env3.5/bin/activate
+    python run.py -e 4 -i ${2}
+  
+END
+  chmod a+x pbs_$2.sh
+}
 
-module load python/3.5.4
-source predicting_high_low_TG_2/predicting_high_low_TG/env3.5/bin/activate
-cd predicting_high_low_TG_2/predicting_high_low_TG/source/range
+delete() {
+  rm pbs_$1.sh
+}
 
-python run.py --n_cpus=27 -e 3
+super() {
+
+    echo $2
+    nick="exp-${2}"
+
+    create $1 $2
+    qsub -N $nick -l select=1:ncpus=$3:mem=$4gb,walltime=$5:00:00 pbs_$2.sh
+    sleep 5
+    delete $2
+}
+
+
+mem=10
+cpus=10
+cd $1 
+
+for i in `seq 0 299`; do
+    while true; do
+
+        SHT=$(totaljob | grep Parallel_short | sed "s/\s\+/|/g" | cut -d"|" -f5)
+        LNG=$(totaljob | grep Parallel_long  | sed "s/\s\+/|/g" | cut -d"|" -f5)
+        RTQ=$(totaljob | grep RouteQ | sed "s/\s\+/|/g" | cut -d"|" -f5)
+
+        if [ "$LNG" -ge $cpus ] && [ "$RTQ" -gt 0 ]; then
+            super $1 ${i} $cpus $mem 336
+            sleep 5
+            break
+        elif [ "$SHT" -ge $cpus ] && [ "$RTQ" -gt 0 ]; then
+            super $1 $2 ${i} $cpus $mem 24
+            sleep 5
+            break
+        else
+            echo "sleeping..."
+            sleep 60
+        fi
+    done
+done
