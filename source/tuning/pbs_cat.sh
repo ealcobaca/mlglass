35e02cf3f6fb5bf4440262c2c781f263a415deb3
e.alcobaca <e.alcobaca@gmail.com>
2019-02-15 17:50:45 -0200

    pbs dt and rf updated

--- source/tuning/pbs_cat.sh
+++ source/tuning/pbs_cat.sh
@@ -8,4 +8,27 @@ cd predicting_high_low_TG/
 source env3.5/bin/activate
 
 cd source/
-make run_tuning_catboost
+
+# uncomment to run
+# make run_tuning_rf input_file=../../data/clean/oxides_ND300_train.csv \ 
+# 	output_folder=../../result/ \
+# 	max_iter=500 \
+# 	seed=500 \
+# 	n_jobs=50 \
+# 	data_tag=nd300 \ 
+#
+# make run_tuning_rf input_file=../../data/clean/oxides_Tliquidus_train.csv \ 
+# 	output_folder=../../result/ \
+# 	max_iter=500 \
+# 	seed=500 \
+# 	n_jobs=50 \
+# 	data_tag=tl \ 
+#
+# make run_tuning_rf input_file=../../data/clean/oxides_Tg_train.csv \ 
+# 	output_folder=../../result/ \
+# 	max_iter=500 \
+# 	seed=500 \
+# 	n_jobs=50 \
+# 	data_tag=tg \ 
+
+
