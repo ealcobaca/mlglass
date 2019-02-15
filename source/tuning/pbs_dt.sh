4018414d98a39fc9b2f5f1ca4ead41b5718e44d8
e.alcobaca <e.alcobaca@gmail.com>
2019-02-15 19:12:44 -0200

    now RF

--- source/tuning/pbs_dt.sh
+++ source/tuning/pbs_dt.sh
@@ -12,8 +12,6 @@ cd source/
 
 # uncomment to run
 
-make run_tuning_dt input_file=../../data/clean/oxides_Tg_train.csv output_folder=../../result/ max_iter=500 seed=500 n_jobs=50 data_tag=tg
-
-make run_tuning_dt input_file=../../data/clean/oxides_ND300_train.csv output_folder=../../result/ max_iter=500 seed=500	n_jobs=50 data_tag=nd300
-
-make run_tuning_dt input_file=../../data/clean/oxides_Tliquidus_train.csv output_folder=../../result/ max_iter=500 seed=500 n_jobs=50 data_tag=tl
+# make run_tuning_dt input_file=../../data/clean/oxides_Tg_train.csv output_folder=../../result/ max_iter=500 seed=500 n_jobs=50 data_tag=tg
+# make run_tuning_dt input_file=../../data/clean/oxides_ND300_train.csv output_folder=../../result/ max_iter=500 seed=500	n_jobs=50 data_tag=nd300
+# make run_tuning_dt input_file=../../data/clean/oxides_Tliquidus_train.csv output_folder=../../result/ max_iter=500 seed=500 n_jobs=50 data_tag=tl
--- source/tuning/pbs_rf.sh
+++ source/tuning/pbs_rf.sh
@@ -10,25 +10,8 @@ source env3.5/bin/activate
 cd source/
 
 # uncomment to run
-make run_tuning_rf input_file=../../data/clean/oxides_ND300_train.csv \ 
-	output_folder=../../result/ \
-	max_iter=500 \
-	seed=500 \
-	n_jobs=50 \
-	data_tag=nd300 \ 
-
-make run_tuning_rf input_file=../../data/clean/oxides_Tliquidus_train.csv \ 
-	output_folder=../../result/ \
-	max_iter=500 \
-	seed=500 \
-	n_jobs=50 \
-	data_tag=tl \ 
-
-make run_tuning_rf input_file=../../data/clean/oxides_Tg_train.csv \ 
-	output_folder=../../result/ \
-	max_iter=500 \
-	seed=500 \
-	n_jobs=50 \
-	data_tag=tg \ 
+# make run_tuning_rf input_file=../../data/clean/oxides_ND300_train.csv output_folder=../../result/ max_iter=500 seed=500 n_jobs=50 data_tag=nd300
+# make run_tuning_rf input_file=../../data/clean/oxides_Tliquidus_train.csv output_folder=../../result/ max_iter=500 seed=500 n_jobs=50 data_tag=tl
+# make run_tuning_rf input_file=../../data/clean/oxides_Tg_train.csv output_folder=../../result/ max_iter=500 seed=500 n_jobs=50 data_tag=tg
 
 
