d7e4fe96f9e8b4c27b1dbbcd76ac8f053d920a4e
smastelini <mastelini@usp.br>
2019-04-03 19:59:36 -0300

    Here we go again (2)

--- source/tuning/cut_train_test.py
+++ source/tuning/cut_train_test.py
@@ -3,16 +3,32 @@ from sklearn.model_selection import KFold
 import numpy as np
 import os
 
-outer_seed = 1
-
-data = pd.read_csv("../../data/clean/data_tg_dupl_rem.csv")
-columns = data.columns
-X, y = data.iloc[:, :-1].values, data.iloc[:, -1].values
-kf = KFold(n_splits=10, random_state=outer_seed, shuffle=True)
-
 directory = "../../data/clean/train_test_split/"
 if not os.path.exists(directory):
     os.makedirs(directory)
+
+outer_seed = 1
+
+data = pd.read_csv("../../data/clean/data_tg_dupl_rem.csv")
+order = np.argsort(data.iloc[:, -1].values).tolist()
+idx_extremes = order[:6]
+idx_extremes.extend(order[-6:])
+
+data_extremes = data.iloc[idx_extremes, :]
+
+idx_not_extremes = [i for i in range(len(data)) if i not in idx_extremes]
+data = data.iloc[idx_not_extremes, :]
+
+data_extremes.to_csv(
+    "../../data/clean/train_test_split/tg_test_extreme.csv",
+    index=False
+)
+
+data.reset_index(drop=True, inplace=True)
+
+columns = data.columns
+X, y = data.iloc[:, :-1].values, data.iloc[:, -1].values
+kf = KFold(n_splits=10, random_state=outer_seed, shuffle=True)
 
 count = 0
 for k, (train_index, test_index) in enumerate(kf.split(X)):
@@ -28,8 +44,8 @@ for k, (train_index, test_index) in enumerate(kf.split(X)):
     df_train.columns = columns
     df_train.to_csv(path_name, index=False)
 
-    idx_max = np.argmax(y_test)
-    idx_min = np.argmin(y_test)
+    # idx_max = np.argmax(y_test)
+    # idx_min = np.argmin(y_test)
 
     path_name = aux.format("test", count)
     df_test = pd.DataFrame(X_test)
@@ -37,10 +53,6 @@ for k, (train_index, test_index) in enumerate(kf.split(X)):
     df_test.columns = columns
     df_test.to_csv(path_name, index=False)
 
-    values.append(np.append(X_test[idx_max], y_test[idx_max]))
-    values.append(np.append(X_test[idx_min], y_test[idx_min]))
-    df_test_extreme = pd.DataFrame(values, columns=columns)
-    df_test_extreme.to_csv(
-        "../../data/clean/train_test_split/tg_test_fold{0:02d}_extreme.csv".format(count),
-        index=False)
-
+    # values.append(np.append(X_test[idx_max], y_test[idx_max]))
+    # values.append(np.append(X_test[idx_min], y_test[idx_min]))
+    # df_test_extreme = pd.DataFrame(values, columns=columns)
