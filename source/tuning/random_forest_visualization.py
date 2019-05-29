770b5ae60d968408ada886a514efe9883fdc8950
smastelini <mastelini@usp.br>
2019-05-29 10:02:29 -0300

    New version of RF visualization

--- source/tuning/random_forest_visualization.py
+++ source/tuning/random_forest_visualization.py
@@ -57,7 +57,7 @@ def get_tree_paths(tree, min, max):
 
 def extract_intervals(rf, min_r, max_r, features_names, resolution=100,
                       range_features=(0, 1)):
-    decay = 0.9
+    decay = 1.0
     incr = (range_features[1] - range_features[0]) / resolution
     n_features = len(features_names) - 1
     paths = []
@@ -113,6 +113,63 @@ def extract_intervals(rf, min_r, max_r, features_names, resolution=100,
     return plot_data, relevances
 
 
+def extract_intervals_with_data(rf, data, min_r, max_r, features_names,
+                                resolution=100, range_features=(0, 1)):
+
+    filter1 = data.iloc[:, -1] >= min_r
+    filter2 = data.iloc[:, -1] <= max_r
+    selected_s = [i for i in range(len(data)) if filter1[i] and filter2[i]]
+
+    incr = (range_features[1] - range_features[0]) / resolution
+    n_features = len(features_names) - 1
+    paths = []
+    for model in rf.estimators_:
+        paths.extend(get_tree_paths(model.tree_, min_r, max_r))
+
+    intervals = [np.zeros(resolution + 1) for i in range(n_features)]
+    for s in selected_s:
+        for path in paths:
+            for elem in path:
+                if elem[0] is not None:
+                    stsfs = (data.iloc[s, elem[1]] <= elem[2]) == elem[0]
+
+                    if stsfs:
+                        point = int(round(elem[2] / incr))
+                        intervals[elem[1]][point] += 1
+    plot_data = {
+        'label': [],
+        'dist': []
+    }
+
+    relevances = []
+    for feat, interval in enumerate(intervals):
+        relevances.append(np.sum(interval))
+        interval_norm = interval.copy()
+
+        max_ = max(interval_norm)
+        min_ = min(interval_norm)
+
+        interval_norm = [
+            int(round(100 * (x - min_)/(max_ - min_))) if max_ > 0 else 0
+            for x in interval_norm
+        ]
+
+        for i, elem in enumerate(interval_norm):
+            plot_data['label'].extend(
+                [features_names[feat] for j in range(int(elem))]
+            )
+            plot_data['dist'].extend(np.repeat(100 * i * incr, elem).tolist())
+
+    max_ = max(relevances)
+    min_ = min(relevances)
+    # relevances = {e: (x - min_)/(max_ - min_)
+    #               for e, x in zip(features_names, relevances)}
+    relevances = [(x - min_)/(max_ - min_) if max_ > 0.0 else 0.0
+                  for x in relevances]
+
+    return plot_data, relevances
+
+
 def plot_violins(plot_data, relevances, filename):
     dt_plot = pd.DataFrame.from_dict(plot_data)
     color_p = sns.cubehelix_palette(start=2, rot=0, dark=0.2, light=1,
@@ -120,19 +177,12 @@ def plot_violins(plot_data, relevances, filename):
     colors = [color_p(r) for r in relevances]
     sns.set(style='whitegrid')
     f, ax = plt.subplots(figsize=(12, 4))
-    # sns.stripplot(x='label', y='dist', data=dt_plot,
-    #               palette=colors, linewidth=0.5, size=0.5, jitter=0.05)
     sns.violinplot(
         x='label', y='dist', data=dt_plot,
         inner=None, palette=colors, linewidth=0.7, cut=0,
-        scale='width', bw=1, gridsize=1000
+        scale='width', bw='silverman', gridsize=1000
     )
     # sns.despine(left=True)
-
-    ##########################################################################
-    # Tiago e Edesio, o parametro bw acima controla a "riqueza de detalhes" na
-    # interpolacao
-    ##########################################################################
 
     ax.set_title('Composition visualization')
     ax.set_xlabel('Features')
@@ -155,13 +205,19 @@ if __name__ == '__main__':
     with open(model_name, 'rb') as f:
         rf = pickle.load(f)
 
-    data = pd.read_csv('{0}/data_tg_dupl_rem.csv'.format(input_path))
+    data = pd.read_csv(
+        '{0}/train_test_split/tg_train_fold04.csv'.format(input_path)
+    )
     features_names = list(data)
 
-    plot_data, relevances = extract_intervals(rf, 1200, 1500, features_names)
+    plot_data, relevances = extract_intervals_with_data(
+        rf, data, 1200, 1500, features_names
+    )
     filename = '{0}/interpretation/rf_vis_high_tg.png'.format(output_path)
     plot_violins(plot_data, relevances, filename)
 
-    plot_data, relevances = extract_intervals(rf, 0, 400, features_names)
+    plot_data, relevances = extract_intervals_with_data(
+        rf, data, 0, 400, features_names
+    )
     filename = '{0}/interpretation/rf_vis_low_tg.png'.format(output_path)
     plot_violins(plot_data, relevances, filename)
